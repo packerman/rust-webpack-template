6bf767f7e857961b0b43981356ffae805fd8cecd
Piotr Ackermann <pioacker@gmail.com>
2022-07-11 23:42:45 +0200

    Migrate to webpack 5 (#1)

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,27 +2,34 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-const dist = path.resolve(__dirname, "dist");
-
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
+    index: "./js/index.js",
   },
   devServer: {
-    contentBase: dist,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+  },
+  devtool: "eval-cheap-module-source-map",
+  performance: {
+    hints: false,
+  },
+  experiments: {
+    // Support the old WebAssembly like in webpack 4.
+    syncWebAssembly: true,
+    // Support the new WebAssembly according to the updated specification (https://github.com/WebAssembly/esm-integration), it makes a WebAssembly module an async module.
+    // asyncWebAssembly: true,
   },
   plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
-
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
+    new CopyPlugin({
+      patterns: [
+        path.resolve(__dirname, "static"),
+      ],
     }),
-  ]
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "."),
+    }),
+  ],
 };
